a945b8f4e9787c6051a3ff7faf6d9dda9af8c07e
indaleto.dev <indaleto.dev@gmail.com>
2018-12-19 02:42:51 +0000

    Pequenas alterações

--- src/routes.php
+++ src/routes.php
@@ -1,16 +1,16 @@
 <?php 
 
-Route::get('configuration', function(){
-	echo 'Hello from the configuration package!';
+
+Route::group(['middleware' => ['web', 'auth']], function(){
+		
+	Route::post('/admin/users/add', 'indaleto\configuration\configurationController@addUser');
+	Route::post('/admin/users/add', 'indaleto\configuration\configurationController@addUser');
+	Route::get('/admin/users/{id}/del', 'indaleto\configuration\configurationController@delUser');
+	Route::post('/admin/users/{id}/edit', 'indaleto\configuration\configurationController@editUser');
+	Route::get('/admin/users/{id}/resetPassword', 'indaleto\configuration\configurationController@sendResetPasswordLink');
+
+	Route::post('/admin/settings/edit','indaleto\configuration\configurationController@editSettings');
+	Route::get('/admin/settings/remLogo','indaleto\configuration\configurationController@remLogo');
 });
-
-Route::post('/admin/users/add', 'indaleto\configuration\configurationController@addUser');
-Route::post('/admin/users/add', 'indaleto\configuration\configurationController@addUser');
-Route::get('/admin/users/{id}/del', 'indaleto\configuration\configurationController@delUser');
-Route::post('/admin/users/{id}/edit', 'indaleto\configuration\configurationController@editUser');
-Route::get('/admin/users/{id}/resetPassword', 'indaleto\configuration\configurationController@sendResetPasswordLink');
-
-Route::post('/admin/settings/edit','indaleto\configuration\configurationController@editSettings');
-Route::get('/admin/settings/remLogo','indaleto\configuration\configurationController@remLogo');
 
 //Route::post('/admin/users/add', 'configurationController@addUser');
